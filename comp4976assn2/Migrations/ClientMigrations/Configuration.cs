bcafb89406dedda974dcf98d581fd43fc4523db7
Brycen Dorsay <dorsay@live.ca>
2015-03-06 02:36:44 -0800

    Smart entity migration configuration

--- comp4976assn2/Migrations/ClientMigrations/Configuration.cs
+++ comp4976assn2/Migrations/ClientMigrations/Configuration.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using comp4976assn2.Models.ClientEntity;
+using comp4976assn2.Models.SmartEntity;
 
 namespace comp4976assn2.Migrations.ClientMigrations
 {
@@ -190,6 +191,175 @@ namespace comp4976assn2.Migrations.ClientMigrations
                 new FileStatusModel { FileStatus = "Reopened" },
                 new FileStatusModel { FileStatus = "Closed" },
                 new FileStatusModel { FileStatus = "Open" }
+            );
+
+            context.SexWorkExploitationModels.AddOrUpdate(
+                s => s.SexWorkExploitation,
+                new SexWorkExploitationModel { SexWorkExploitation = "Yes" },
+                new SexWorkExploitationModel { SexWorkExploitation = "No" },
+                new SexWorkExploitationModel { SexWorkExploitation = "N/A" }
+            );
+
+            context.MultiplePerpetratorsModels.AddOrUpdate(
+                m => m.MultiplePerpetrators,
+                new MultiplePerpetratorsModel { MultiplePerpetrators = "Yes" },
+                new MultiplePerpetratorsModel { MultiplePerpetrators = "No" },
+                new MultiplePerpetratorsModel { MultiplePerpetrators = "N/A" }
+            );
+
+            context.CityOfAssaultModels.AddOrUpdate(
+                c => c.CityOfAssault,
+                new CityOfAssaultModel { CityOfAssault = "Surrey" },
+                new CityOfAssaultModel { CityOfAssault = "Abbotsford" },
+                new CityOfAssaultModel { CityOfAssault = "Agassiz" },
+                new CityOfAssaultModel { CityOfAssault = "Boston Bar" },
+                new CityOfAssaultModel { CityOfAssault = "Burnaby" },
+                new CityOfAssaultModel { CityOfAssault = "Burnaby" },
+                new CityOfAssaultModel { CityOfAssault = "Coquitlam" },
+                new CityOfAssaultModel { CityOfAssault = "Delta" },
+                new CityOfAssaultModel { CityOfAssault = "Harrison Hot Springs" },
+                new CityOfAssaultModel { CityOfAssault = "Hope" },
+                new CityOfAssaultModel { CityOfAssault = "Langley" },
+                new CityOfAssaultModel { CityOfAssault = "Maple Ridge" },
+                new CityOfAssaultModel { CityOfAssault = "Mission" },
+                new CityOfAssaultModel { CityOfAssault = "New Westminster" },
+                new CityOfAssaultModel { CityOfAssault = "Pitt Meadows" },
+                new CityOfAssaultModel { CityOfAssault = "Port Coquitlam" },
+                new CityOfAssaultModel { CityOfAssault = "Port Moody" },
+                new CityOfAssaultModel { CityOfAssault = "Vancouver" },
+                new CityOfAssaultModel { CityOfAssault = "Yale" },
+                new CityOfAssaultModel { CityOfAssault = "Other - BC" },
+                new CityOfAssaultModel { CityOfAssault = "Out-of-Province" },
+                new CityOfAssaultModel { CityOfAssault = "International" }
+            );
+
+            context.CityOfResidenceModels.AddOrUpdate(
+                c => c.CityOfResidence,
+                new CityOfResidenceModel { CityOfResidence = "Surrey" },
+                new CityOfResidenceModel { CityOfResidence = "Abbotsford" },
+                new CityOfResidenceModel { CityOfResidence = "Agassiz" },
+                new CityOfResidenceModel { CityOfResidence = "Boston Bar" },
+                new CityOfResidenceModel { CityOfResidence = "Burnaby" },
+                new CityOfResidenceModel { CityOfResidence = "Burnaby" },
+                new CityOfResidenceModel { CityOfResidence = "Coquitlam" },
+                new CityOfResidenceModel { CityOfResidence = "Delta" },
+                new CityOfResidenceModel { CityOfResidence = "Harrison Hot Springs" },
+                new CityOfResidenceModel { CityOfResidence = "Hope" },
+                new CityOfResidenceModel { CityOfResidence = "Langley" },
+                new CityOfResidenceModel { CityOfResidence = "Maple Ridge" },
+                new CityOfResidenceModel { CityOfResidence = "Mission" },
+                new CityOfResidenceModel { CityOfResidence = "New Westminster" },
+                new CityOfResidenceModel { CityOfResidence = "Pitt Meadows" },
+                new CityOfResidenceModel { CityOfResidence = "Port Coquitlam" },
+                new CityOfResidenceModel { CityOfResidence = "Port Moody" },
+                new CityOfResidenceModel { CityOfResidence = "Vancouver" },
+                new CityOfResidenceModel { CityOfResidence = "Yale" },
+                new CityOfResidenceModel { CityOfResidence = "Other - BC" },
+                new CityOfResidenceModel { CityOfResidence = "Out-of-Province" },
+                new CityOfResidenceModel { CityOfResidence = "International" }
+            );
+
+            context.ReferringHospitalModels.AddOrUpdate(
+                r => r.ReferringHospital,
+                new ReferringHospitalModel { ReferringHospital = "Abbotsford Regional Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Surrey Memorial Hosptical" },
+                new ReferringHospitalModel { ReferringHospital = "Burnaby Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Chilliwack General Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Delta Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Eagle Ridge Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Fraser Canyon Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Langley Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Mission Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Peace Arch Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Ridge Meadows Hospital" },
+                new ReferringHospitalModel { ReferringHospital = "Royal Columbia Hospital" }
+            );
+
+            context.HospitalAttendedModels.AddOrUpdate(
+                r => r.HospitalAttended,
+                new HospitalAttendedModel { HospitalAttended = "Abbotsford Regional Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Surrey Memorial Hosptical" },
+                new HospitalAttendedModel { HospitalAttended = "Burnaby Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Chilliwack General Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Delta Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Eagle Ridge Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Fraser Canyon Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Langley Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Mission Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Peace Arch Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Ridge Meadows Hospital" },
+                new HospitalAttendedModel { HospitalAttended = "Royal Columbia Hospital" }
+            );
+
+            context.SocialWorkAttendanceModels.AddOrUpdate(
+                s => s.SocialWorkAttendance,
+                new SocialWorkAttendanceModel { SocialWorkAttendance = "Yes" },
+                new SocialWorkAttendanceModel { SocialWorkAttendance = "No" },
+                new SocialWorkAttendanceModel { SocialWorkAttendance = "N/A" }
+            );
+
+            context.PoliceAttendanceModels.AddOrUpdate(
+                s => s.PoliceAttendance,
+                new PoliceAttendanceModel { PoliceAttendance = "Yes" },
+                new PoliceAttendanceModel { PoliceAttendance = "No" },
+                new PoliceAttendanceModel { PoliceAttendance = "N/A" }
+            );
+
+            context.VictimServicesAttendanceModels.AddOrUpdate(
+                s => s.VictimServicesAttendance,
+                new VictimServicesAttendanceModel { VictimServicesAttendance = "Yes" },
+                new VictimServicesAttendanceModel { VictimServicesAttendance = "No" },
+                new VictimServicesAttendanceModel { VictimServicesAttendance = "N/A" }
+            );
+
+            context.MedicalOnlyModels.AddOrUpdate(
+                s => s.MedicalOnly,
+                new MedicalOnlyModel { MedicalOnly = "Yes" },
+                new MedicalOnlyModel { MedicalOnly = "No" },
+                new MedicalOnlyModel { MedicalOnly = "N/A" }
+            );
+
+            context.EvidenceStoredModels.AddOrUpdate(
+                s => s.EvidenceStored,
+                new EvidenceStoredModel { EvidenceStored = "Yes" },
+                new EvidenceStoredModel { EvidenceStored = "No" },
+                new EvidenceStoredModel { EvidenceStored = "N/A" }
+            );
+
+            context.HivMedsModels.AddOrUpdate(
+                s => s.HIVMeds,
+                new HIVMedsModel { HIVMeds = "Yes" },
+                new HIVMedsModel { HIVMeds = "No" },
+                new HIVMedsModel { HIVMeds = "N/A" }
+            );
+
+            context.ReferredToCbvsModels.AddOrUpdate(
+                s => s.ReferredToCBVS,
+                new ReferredToCBVSModel { ReferredToCBVS = "Yes" },
+                new ReferredToCBVSModel { ReferredToCBVS = "No" },
+                new ReferredToCBVSModel { ReferredToCBVS = "PVBS Only" },
+                new ReferredToCBVSModel { ReferredToCBVS = "N/A" }
+            );
+
+            context.PoliceReportedModels.AddOrUpdate(
+                s => s.PoliceReported,
+                new PoliceReportedModel { PoliceReported = "Yes" },
+                new PoliceReportedModel { PoliceReported = "No" },
+                new PoliceReportedModel { PoliceReported = "N/A" }
+            );
+
+            context.ThirdPartyReportModels.AddOrUpdate(
+                s => s.ThirdPartyReport,
+                new ThirdPartyReportModel { ThirdPartyReport = "Yes" },
+                new ThirdPartyReportModel { ThirdPartyReport = "No" },
+                new ThirdPartyReportModel { ThirdPartyReport = "N/A" }
+            );
+
+            context.BadDateReportModels.AddOrUpdate(
+                s => s.BadDateReport,
+                new BadDateReportModel { BadDateReport = "Yes" },
+                new BadDateReportModel { BadDateReport = "No" },
+                new BadDateReportModel { BadDateReport = "N/A" }
             );
         }
     }
